c81d2351c585ff8cee56d1090f7ffc1be27bdbf8
Gleb Laychenkov <racers2no95@mail.ru>
2024-05-12 22:54:07 +0400

    commit_1

--- src/test/java/ru/netology/SelenideTest.java
+++ src/test/java/ru/netology/SelenideTest.java
@@ -3,12 +3,10 @@ package ru.netology;
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
